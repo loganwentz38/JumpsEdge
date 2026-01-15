69e52d19899ffcf2b31e0463fa577436cac62e2d
loganwentz38 <113048686+loganwentz38@users.noreply.github.com>
2026-01-15 12:00:14 -0600

    Initial Commit

---
+++ JumpersEdgeTests/JumpersEdgeTests.swift
@@ -0,0 +1,17 @@
+//
+//  JumpersEdgeTests.swift
+//  JumpersEdgeTests
+//
+//  Created by Logan Wentz on 1/15/26.
+//
+
+import Testing
+@testable import JumpersEdge
+
+struct JumpersEdgeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
